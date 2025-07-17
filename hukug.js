3a734fd879c6b8f8889c0f96d2c37ee60172fc26
laisamorais <cuchinatalia27@gmail.com>
2025-07-17 09:24:03 -0300

    Add files via upload

---
+++ hukug.js
@@ -0,0 +1,20 @@
+const num = [3, 7, 18, 21, 33]
+console.log(num)
+
+
+
+function inserirFinalArray(){
+    var novoNum = prompt("digite um novo numero: ")
+    num.push(novoNum)
+    console.log(num)
+}
+   function calcularMediaArray(){
+    var soma = 0, media = 0
+    for(i=0, numeros.length; 1+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           +){
+        var num = numeros[i]
+
+    }
+
+media = soma / numeros.length
+alert(media)
+}
